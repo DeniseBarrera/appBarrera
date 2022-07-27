a484ff856c1a3b7428863cdf555c501fcb8c7aa9
Denise Barrera <denise.barrera@outlook.com.ar>
2022-07-27 12:26:20 -0300

    desafio2

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+    return (
+        <header id="header">
+                <h1 className="titulo">Tienda Mía</h1>
+                <nav>
+                    <a href="#">link</a>
+                </nav>
+            </header>
+    )
+}
+
+export default NavBar
