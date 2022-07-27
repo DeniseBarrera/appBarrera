fd5c0efb5ca6a1fb131e5f9aedb099def7d034f6
DeniseBarrera <100180533+DeniseBarrera@users.noreply.github.com>
2022-07-27 12:30:55 -0300

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Header from "./NavBar";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <main>
+                <h2>Inicio</h2>
+            </main>
+            <footer>
+                <p>footer</p>
+            </footer>
+        </>
+    )
+}
+
+export default App;
